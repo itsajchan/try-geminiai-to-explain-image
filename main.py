fcbb505bd5ce848f1018d4d0fafcb714564f20bb
itsajchan <adam@ajchan.io>
2023-12-19 12:09:41 -0800

    Add main.py that loads ArduinoStarterKit.webp and calls gemini-pro-vision

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+import google.generativeai as genai
+import PIL.Image
+
+img = PIL.Image.open('ArduinoStarterKit.webp')
+
+from dotenv import load_dotenv
+load_dotenv()
+
+GEMINI_API_KEY=os.getenv('GEMINI_API_KEY')
+genai.configure(api_key=GEMINI_API_KEY)
+
+model = genai.GenerativeModel('gemini-pro-vision')
+response = model.generate_content(["Explain the different components seen in this image.", img])
+response.resolve()
+
+print(response.text)
